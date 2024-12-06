8a42bb25c660bc824c26f7105ec1a660467de3d7
SHIVAM RAWAT <117502407+Shivam92003@users.noreply.github.com>
2024-12-06 15:32:22 +0530

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -61,7 +61,7 @@ const App = () => {
           <Route path="/bikes" element={<BikeList />} />
         </Routes>
       </div>
-    </Router>
+    </Router >
   );
 };
 
