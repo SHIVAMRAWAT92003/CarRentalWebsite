68487d96372df3b679229ad38ff4841dc7301c8b
SHIVAM RAWAT <117502407+Shivam92003@users.noreply.github.com>
2024-12-06 14:27:46 +0530

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/CarRentalWebsite" >
       <div className="bg-white dark:bg-black dark:text-white text-black overflow-x-hidden">
         <Navbar theme={theme} setTheme={setTheme} />
         <Routes>
